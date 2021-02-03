6cc2d3e698f2be7aec27a50a711248886a95c858
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-04 00:13:48 +0700

    Any q-ons.

--- src/main/kotlin/InterfaceAttach.kt
+++ src/main/kotlin/InterfaceAttach.kt
@@ -2,15 +2,16 @@ interface InterfaceAttach {
     val type: String
 }
 
-class Photo(val id: Int,
-            val ownId: Int,
-            val urlPreview: String,
-            val urlFullSize: String) : InterfaceAttach {
-
+class PhotoAttach(val id: Int,
+                  val ownId: Int,
+                  val urlPreview: String,
+                  val urlFullSize: String) : InterfaceAttach, Photo() {
     override val type = "photo"
+    val photo: Photo = Photo()
+
     override fun toString(): String {
-        return """|
-            |Формат вложения: $type 
+        return """| 
+            | Тип вложения: $type
             |   id: $id
             |   ownId: $ownId
             |   urlPreview: $urlPreview
@@ -18,75 +19,77 @@ class Photo(val id: Int,
     }
 }
 
-class Audio(val id: Int,
-            val ownId: Int,
-            val artist: String,
-            val title: String,
-            val duration: Int,
-            val urlMp3: String) : InterfaceAttach {
+open class Photo()
 
-    override val type = "audio"
-    override fun toString(): String {
-        return """|
-            |Формат вложения: $type 
-            |   id: $id
-            |   ownId: $ownId
-            |   artist: $artist
-            |   title: $title
-            |   duration: $duration
-            |   urlMp3: $urlMp3""".trimMargin()
-    }
-}
-
-class Video(val id: Int,
-            val ownId: Int,
-            val title: String,
-            val description: String,
-            val duration: Int) : InterfaceAttach {
-    override val type = "video"
-    override fun toString(): String {
-        return """|
-            |Формат вложения: $type 
-            |   id: $id
-            |   ownId: $ownId
-            |   title: $title
-            |   description: $description
-            |   duration: $duration""".trimMargin()
-    }
-}
-
-class Document(val id: Int,
-            val ownId: Int,
-            val title: String,
-            val size: Int,
-            val ext: Int,
-            val date: Int,
-            val types: Int) : InterfaceAttach {
-    override val type = "document"
-    override fun toString(): String {
-        return """|
-            |Формат вложения: $type 
-            |   id: $id
-            |   ownId: $ownId
-            |   title: $title
-            |   size: $size
-            |   ext: $ext
-            |   date: $date
-            |   types: $types""".trimMargin()
-    }
-}
-
-class Graffiti(val id: Int,
-             val ownId: Int,
-             val urlPreview: String,
-             val urlFullSize: String) : InterfaceAttach {
-    override val type = "graffiti"
-    override fun toString(): String {
-        return """|
-            |Формат вложения: $type 
-            |   id: $id
-            |   ownId: $ownId
-            |   urlPreview: $urlPreview
-            |   urlFullSize: $urlFullSize""".trimMargin()
-    }
-}
+//class Audio(val id: Int,
+//            val ownId: Int,
+//            val artist: String,
+//            val title: String,
+//            val duration: Int,
+//            val urlMp3: String) : InterfaceAttach {
+//
+//    override val type = "audio"
+//    override fun toString(): String {
+//        return """|
+//            |Формат вложения: $type
+//            |   id: $id
+//            |   ownId: $ownId
+//            |   artist: $artist
+//            |   title: $title
+//            |   duration: $duration
+//            |   urlMp3: $urlMp3""".trimMargin()
+//    }
+//}
+//
+//class Video(val id: Int,
+//            val ownId: Int,
+//            val title: String,
+//            val description: String,
+//            val duration: Int) : InterfaceAttach {
+//    override val type = "video"
+//    override fun toString(): String {
+//        return """|
+//            |Формат вложения: $type
+//            |   id: $id
+//            |   ownId: $ownId
+//            |   title: $title
+//            |   description: $description
+//            |   duration: $duration""".trimMargin()
+//    }
+//}
+//
+//class Document(val id: Int,
+//            val ownId: Int,
+//            val title: String,
+//            val size: Int,
+//            val ext: Int,
+//            val date: Int,
+//            val types: Int) : InterfaceAttach {
+//    override val type = "document"
+//    override fun toString(): String {
+//        return """|
+//            |Формат вложения: $type
+//            |   id: $id
+//            |   ownId: $ownId
+//            |   title: $title
+//            |   size: $size
+//            |   ext: $ext
+//            |   date: $date
+//            |   types: $types""".trimMargin()
+//    }
+//}
+//
+//class Graffiti(val id: Int,
+//             val ownId: Int,
+//             val urlPreview: String,
+//             val urlFullSize: String) : InterfaceAttach {
+//    override val type = "graffiti"
+//    override fun toString(): String {
+//        return """|
+//            |Формат вложения: $type
+//            |   id: $id
+//            |   ownId: $ownId
+//            |   urlPreview: $urlPreview
+//            |   urlFullSize: $urlFullSize""".trimMargin()
+//    }
+//}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,20 @@
 fun main(args: Array<String>) {
 
-    val photo: InterfaceAttach = Photo(1, 1, "urlPreview", "urlFullSize")
-    val audio: InterfaceAttach = Audio(1, 1, "artist", "title", 100, "urlMp3")
+    val photo: Photo = Photo()
 
-    println(photo)
+    val photo_1: Photo = PhotoAttach(1, 1, "1_dfs", "1_we")
+    val photo_2: Photo = PhotoAttach(2, 2, "2_dfs", "2_we")
+
+    // Проверка вывода в консоль
+    println(photo_1 is InterfaceAttach)
+    println(photo_1 is PhotoAttach)
+
+
     println("**********************")
-    var attachments = arrayOf(photo, audio)
-    println(attachments.contentToString())
-
+    var attachments = arrayOf(photo_1, photo_2)
+//    println(attachments.contentToString())
+    for(attach in attachments) {
+        if(attach is PhotoAttach)
+            println(attach)
+    }
 }
